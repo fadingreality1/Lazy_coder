43b559b148230aa62a552b4e01c38b4e478a31c7
fadingreality1 <kunalverma.learn@gmail.com>
2023-06-08 13:44:44 +0530

    contact page optimization

--- home/forms.py
+++ home/forms.py
@@ -3,15 +3,27 @@ from .models import Contact
 
 
 class contactForm(forms.ModelForm):
-    name = forms.CharField(label="" ,max_length=100, required=True,widget=forms.TextInput(attrs={"class": "full-width", "placeholder":"Your Name"}),)
-
-    email = forms.EmailField(label="" ,max_length=254, required=True,widget=forms.EmailInput(attrs={"class": "full-width", "placeholder":"Your Email"}),)
-
-    website = forms.URLField(label="" ,max_length=200, required=False,widget=forms.TextInput(attrs={"class": "full-width", "placeholder":"Website"}),)
-
-    message = forms.CharField(label="" ,widget=forms.Textarea(attrs={"class": "full-width", "placeholder": "Your Message"}),) 
+    # ! all parameter defined to apply styling over the page
     
-    phone = forms.CharField(label="", required=False, widget=forms.TextInput(attrs={"class": "full-width", "placeholder":"Your Phone number with country code (+XX XXX XXX XXXX)", "type": "tel",}),)
+    name = forms.CharField(label="Name" ,max_length=100, required=True,widget=forms.TextInput(attrs={"class": "full-width", "placeholder":"Your Name"}),)
+
+    email = forms.EmailField(label="Email" ,max_length=254, required=True,widget=forms.EmailInput(attrs={"class": "full-width", "placeholder":"Your Email"}),)
+
+    website = forms.URLField(label="Website" ,max_length=200, required=False,widget=forms.TextInput(attrs={"class": "full-width", "placeholder":"Website"}),)
+
+    message = forms.CharField(label="Message" ,widget=forms.Textarea(attrs={"class": "full-width", "placeholder": "Your Message"}),) 
+    
+    phone = forms.CharField(label="Phone number", required=False, widget=forms.TextInput(attrs={"class": "full-width", "placeholder":"Your Phone number with country code (+XX XXX XXX XXXX)", "type": "tel",}),)
+    
+    # name = forms.CharField(label="" ,max_length=100, required=True,widget=forms.TextInput(attrs={"class": "full-width", "placeholder":"Your Name"}),)
+
+    # email = forms.EmailField(label="" ,max_length=254, required=True,widget=forms.EmailInput(attrs={"class": "full-width", "placeholder":"Your Email"}),)
+
+    # website = forms.URLField(label="" ,max_length=200, required=False,widget=forms.TextInput(attrs={"class": "full-width", "placeholder":"Website"}),)
+
+    # message = forms.CharField(label="" ,widget=forms.Textarea(attrs={"class": "full-width", "placeholder": "Your Message"}),) 
+    
+    # phone = forms.CharField(label="", required=False, widget=forms.TextInput(attrs={"class": "full-width", "placeholder":"Your Phone number with country code (+XX XXX XXX XXXX)", "type": "tel",}),)
         
     class Meta:
         model = Contact
