6f665a8e256f5862272c2ec6b4889e29fd36e22d
fadingreality1 <kunalverma.learn@gmail.com>
2023-06-20 15:56:03 +0530

    changes db successfully

--- staticfiles/Lazy_coder/settings.8e309bdbe357.py
+++ staticfiles/Lazy_coder/settings.8e309bdbe357.py
@@ -58,22 +58,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Lazy_coder.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': config('DB_NAME'),
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASS'),
-#         'HOST': config('DB_HOST'),
-#         'PORT': config('DB_PORT'),
-#         'OPTIONS': {'sslmode': 'require'},
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASS'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+        'OPTIONS': {'sslmode': 'require'},
     }
 }
 
